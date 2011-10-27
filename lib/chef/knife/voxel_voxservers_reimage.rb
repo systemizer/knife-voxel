3966c82490c8bc1b01ae1853e7e318cbe0715648
James W. Brinkerhoff <jwb@paravolve.net>
2011-10-27 16:20:04 -0400

    0.0.8: Added more detail after voxcloud create and voxservers reimage

--- lib/chef/knife/voxel_voxservers_reimage.rb
+++ lib/chef/knife/voxel_voxservers_reimage.rb
@@ -170,8 +170,16 @@ class Chef
 
               bootstrap_for_node(device).run
 
-              puts "#{ui.color("Environment", :cyan)}: #{config[:environment] || '_default'}"
               puts "#{ui.color("Run List", :cyan)}: #{config[:run_list].join(', ')}"
+              puts "#{ui.color("Node Name", :cyan)}: #{config[:node_name] || 'd' + device['id']}"
+              puts "#{ui.color("Environment", :cyan)}: #{config[:environment] || '_default'}"
+              puts "#{ui.color("Device ID", :cyan)}: #{device['id']}"
+              puts "#{ui.color("Name", :cyan)}: #{device['label']}"
+              puts "#{ui.color("Image Id", :cyan)}: #{config[:image_id]}"
+              puts "#{ui.color("Facility", :cyan)}: #{device['location']['facility']['code']}"
+              puts "#{ui.color("Public IP Address", :cyan)}: #{device['ipassignments']['ipassignment'].select { |i| i['type'] == 'frontend' }.first['content']}"
+              puts "#{ui.color("Private IP Address", :cyan)}: #{device['ipassignments']['ipassignment'].select { |i| i['type'] == 'backend' }.first['content']}"
+              puts "#{ui.color("Root Password", :cyan)}: #{device['accessmethods']['accessmethod'].select { |a| a['type'] == 'admin' }.first['password']}"
             end
           end
         else
